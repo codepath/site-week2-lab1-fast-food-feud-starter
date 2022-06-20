7d4c2200020fb294a79aff4e07adac43c8563296
Eddy Ogola Onyango <ogolaedd@fb.com>
2022-06-20 15:17:53 -0700

    Add onClick handle

--- src/components/Chip/Chip.jsx
+++ src/components/Chip/Chip.jsx
@@ -1,9 +1,9 @@
 import * as React from "react"
 import "./Chip.css"
 
-export function Chip({ label = "", isActive = false }) {
+export function Chip({ label = "", isActive = false, onClick }) {
   return (
-    <button className={"chip" + (isActive ? " active": "")}>
+    <button className={"chip" + (isActive ? " active": "")} onClick={onClick}>
       <p className="label">{label}</p>
       <span className="close" role="button">{`X`}</span>
     </button>
