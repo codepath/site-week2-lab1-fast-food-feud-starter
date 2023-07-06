35a40cfc64dbdc5cab0f3d511b805c3a144fc407
Yvonne Ariri <yvonneariri@meta.com>
2023-07-06 11:59:49 -0700

    practice

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,13 +1,13 @@
-import "./Header.css"
+import "./Header.css";
 
 export function Header(props) {
   return (
     <header className="header">
-      <h1 className="title"></h1>
-      <h4 className="tagline"></h4>
-      <p className="description"></p>
+      <h1 className="title">{props.title}</h1>
+      <h4 className="tagline">{props.tagline}</h4>
+      <p className="description">{props.description}</p>
     </header>
-  )
+  );
 }
 
-export default Header
+export default Header;
